c47d416dc12b40c8e101c9cd1a190fd768ed11c7
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2026-03-15 16:41:18 +0100

    Added guard enemy structure

--- Assets/Scripts/Framework/BehaviourTreeSystem/Nodes/ConditionalNode.cs
+++ Assets/Scripts/Framework/BehaviourTreeSystem/Nodes/ConditionalNode.cs
@@ -3,6 +3,7 @@
     public class ConditionalNode : Node
     {
         private bool _condition;
+        private string _conditionName = string.Empty;
         private Node _node;
         
         public ConditionalNode(bool condition, Node node)
@@ -11,10 +12,19 @@
             _node = node;
         }
 
+        public ConditionalNode(string conditionName, Node node)
+        {
+            _conditionName = conditionName;
+            _node = node;
+        }
+        
         public void UpdateCondition(bool newCondition) => _condition = newCondition;
         
         protected override NodeStatus OnUpdate()
         {
+            if (_conditionName != string.Empty)
+                _condition = p_dictWrapper.Get<bool>(_conditionName);
+            
             return _condition ? _node.Update() : NodeStatus.SUCCES;
         }
         
---
+++ Assets/Scripts/NPC/GuardEnemy.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+using Framework.BehaviourTreeSystem;
+using Framework.BehaviourTreeSystem.Nodes;
+using Framework.BehaviourTreeSystem.Nodes.TaskNodes;
+
+namespace NPC
+{
+    public sealed class GuardEnemy : MonoBehaviour
+    {
+        [SerializeField] private float speed = 1;
+        [SerializeField] private bool see;
+        [SerializeField] private bool weapon;
+        [SerializeField] private bool range;
+        
+        private Node _tree;
+        private Node _patrol;
+        private Node _attack;
+
+        DictWrapper dictWrapper = new ();
+        
+        private bool _canSeePlayer;
+        private bool _hasWeapon;
+        private bool _isInRange;
+
+        private void Start()
+        {
+            Node screachWeapon = new TestNode("");
+            Vector2 playerPos = Vector2.zero;
+
+            dictWrapper.Set("canSeePlayer", false);
+            dictWrapper.Set("hasWeapon", false);
+            dictWrapper.Set("isInRange", false);
+            
+            // todo: make it work, this is only structure
+            _attack = new SelectorNode(
+                new InvertNode(new ConditionalNode("canSeePlayer", new ConditionalNode("hasWeapon", screachWeapon))),
+                new InvertNode(new ConditionalNode("isInRange", new MoveNode(gameObject, playerPos, speed))),
+                new TestNode("Make an attack node")
+            );
+
+            // todo: fix patrol
+            _patrol = new SequenceNode();
+            
+            _tree = new SelectorNode(
+                _attack,
+                _patrol
+            );
+            
+            _tree.SetDictWrapper(dictWrapper);
+        }
+
+        private void Update()
+        {
+            // todo: update all bools
+            dictWrapper.Set("canSeePlayer", see);
+            dictWrapper.Set("hasWeapon", weapon);
+            dictWrapper.Set("isInRange", range);
+            
+            _tree.Update();
+        }
+    }
+}
