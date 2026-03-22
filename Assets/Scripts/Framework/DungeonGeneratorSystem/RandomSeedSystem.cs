653b8a83a4855840d2f07d3189c99d0abcb7a5fa
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2026-03-22 19:04:08 +0100

    Added randomized number generation via seed

---
+++ Assets/Scripts/Framework/DungeonGeneratorSystem/RandomSeedSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Framework.DungeonGeneratorSystem
+{
+    public static class RandomSeedSystem
+    {
+        private static int _seed;
+        private static Random _random;
+
+        public static void SetSeed(string seed)
+        {
+            _seed = seed.GetHashCode();
+            _random = new (_seed);
+        }
+
+        public static double GetRandomDouble() => _random.NextDouble();
+        
+        public static int GetRandomInt() => _random.Next();
+
+        public static float GetRandomFloat() => (float) GetRandomDouble();
+    }
+}
---
+++ Assets/Scripts/Framework/DungeonGeneratorSystem/TestSeed.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Framework.DungeonGeneratorSystem
+{
+    public class TestSeed : MonoBehaviour
+    {
+        [SerializeField] private string seed;
+        [SerializeField] private SpriteRenderer[] sprites;
+
+        private void Start() => SetRandomColors();
+
+        [ContextMenu("Do random colors")]
+        private void SetRandomColors()
+        {
+            RandomSeedSystem.SetSeed(seed);
+
+            foreach (SpriteRenderer sprite in sprites)
+            {
+                sprite.color = new (RandomSeedSystem.GetRandomFloat(), RandomSeedSystem.GetRandomFloat(), RandomSeedSystem.GetRandomFloat());
+            }
+        }
+    }
+}
